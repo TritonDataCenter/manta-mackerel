5bd3854ddd3939f46f803f97ec3fc2853ae9a2bc
Fred Kuo <fred.kuo@joyent.com>
2013-08-30 21:27:14 +0000

    [minor] set cron scripts to retry

--- scripts/cron/meter-hourly.sh
+++ scripts/cron/meter-hourly.sh
@@ -8,7 +8,7 @@ getDate "$@"
 
 NODE=$dir/../../build/node/bin/node
 
-$NODE $dir/../../bin/meter -j 'storage' -d "$date"
-$NODE $dir/../../bin/meter -j 'request' -d "$date"
-$NODE $dir/../../bin/meter -j 'compute' -d "$date"
-$NODE $dir/../../bin/meter -j 'accessLogs' -d "$date"
+$NODE $dir/../../bin/meter -j 'storage' -d "$date" -r
+$NODE $dir/../../bin/meter -j 'request' -d "$date" -r
+$NODE $dir/../../bin/meter -j 'compute' -d "$date" -r
+$NODE $dir/../../bin/meter -j 'accessLogs' -d "$date" -r
