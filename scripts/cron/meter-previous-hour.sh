7d9141292a0a16edde573d441d9eb4d47dfc4b40
Fred Kuo <fred.kuo@joyent.com>
2014-05-29 23:07:22 +0000

    MANTA-2251 Metering needs to handle the fact that pg dumps arrive ~2-3 hours later than they used to

--- scripts/cron/meter-previous-hour.sh
+++ scripts/cron/meter-previous-hour.sh
@@ -2,4 +2,14 @@
 # Copyright (c) 2013, Joyent, Inc. All rights reserved.
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$dir/meter-hourly.sh `date -d "1 hour ago"`
+source $dir/common.sh
+
+date=$(date --utc -d '1 hour ago')
+storagedate=$(date --utc -d '4 hours ago')
+
+NODE=$dir/../../build/node/bin/node
+
+$NODE $dir/../../bin/meter -j 'storage' -d "$storagedate" -r
+$NODE $dir/../../bin/meter -j 'request' -d "$date" -r
+$NODE $dir/../../bin/meter -j 'compute' -d "$date" -r
+$NODE $dir/../../bin/meter -j 'accessLogs' -d "$date" -r
