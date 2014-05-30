576308d222ccb103b05c6c622bad7dd7afe81c1e
Yunong Xiao <yunong@joyent.com>
2014-05-30 23:43:20 +0000

    MANATEE-193 start-job was not calculating marlin job disk size correctly

--- scripts/pg_process/start-job.sh
+++ scripts/pg_process/start-job.sh
@@ -11,39 +11,38 @@ disk=
 
 calc_disk_size ()
 {
-    local size=$1
-    if [[ $size -le 2 ]]
+    if [[ $1 -le 2 ]]
     then
         disk=2
-    elif [[ $size -le 4 ]]
+    elif [[ $1 -le 4 ]]
     then
         disk=4
-    elif [[ $size -le 8 ]]
+    elif [[ $1 -le 8 ]]
     then
         disk=8
-    elif [[ $size -le 16 ]]
+    elif [[ $1 -le 16 ]]
     then
         disk=16
-    elif [[ $size -le 32 ]]
+    elif [[ $1 -le 32 ]]
     then
         disk=32
-    elif [[ $size -le 64 ]]
+    elif [[ $1 -le 64 ]]
     then
         disk=64
-    elif [[ $size -le 128 ]]
+    elif [[ $1 -le 128 ]]
     then
         disk=128
-    elif [[ $size -le 256 ]]
+    elif [[ $1 -le 256 ]]
     then
         disk=256
-    elif [[ $size -le 512 ]]
+    elif [[ $1 -le 512 ]]
     then
         disk=512
-    elif [[ $size -le 1024 ]]
+    elif [[ $1 -le 1024 ]]
     then
         disk=1024
     else
-        echo "dump size $size exceeds marlin supported disk size"
+        echo "dump size $1 exceeds marlin supported disk size"
         exit 1
     fi
 }
@@ -56,11 +55,11 @@ for s in $shards
 do
     dump=$(mfind -t o /poseidon/stor/manatee_backups/$s/$date | \
         grep moray- | tail -1)
-    bytes=$(minfo $dump | grep m-pg-size | cut -d ' ' -f2)
+    kbytes=$(minfo $dump | grep m-pg-size | cut -d ' ' -f2)
     # Allocate at least 1GB or 4x what the DB dir size was.
-    gbytes=$(echo "scale=0;  $bytes / 2^20 * 4 + 1" | bc)
+    gbytes=$(echo "scale=0;  $kbytes / 2^20 * 4 + 1" | bc)
     # DB size is appx 10x what the compressed dump is.
-    calc_disk_size $gbyte
+    calc_disk_size $gbytes
 
     echo $dump | mjob create --memory=8192 --disk=$disk  -s \
         /poseidon/stor/pgdump/assets/postgresql.conf -s \
