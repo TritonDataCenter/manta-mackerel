e9c320e2a21399b22d5d19cd18c4a927ba32538b
Fred Kuo <fred.kuo@joyent.com>
2014-09-02 16:06:57 +0000

    TOOLS-607 missed licenses on a few scripts

--- scripts/cron/meter-previous-day.sh
+++ scripts/cron/meter-previous-day.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# Copyright (c) 2013, Joyent, Inc. All rights reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+#
+# Copyright (c) 2014, Joyent, Inc.
+#
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $dir/meter-daily.sh `date -d "1 day ago"`
