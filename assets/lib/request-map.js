51f3f0ef947ad60c40cfa8094ec563847d06ae95
Fred Kuo <fred.kuo@joyent.com>
2014-12-31 22:46:04 +0000

    MANTA-2531 request metering overhaul

--- assets/lib/request-map.js
+++ assets/lib/request-map.js
@@ -6,195 +6,231 @@
  */
 
 /*
- * Copyright (c) 2014, Joyent, Inc.
+ * Copyright (c) 2015, Joyent, Inc.
  */
 
-var mod_carrier = require('carrier');
 var Big = require('big.js');
-var ERROR = false;
-var lookupPath = process.env['LOOKUP_FILE'] || '../etc/lookup.json';
-var lookup = require(lookupPath); // maps uuid->approved_for_provisioning
+var Transform = require('stream').Transform;
+var lstream = require('lstream');
+var util = require('util');
 
-var COUNT_UNAPPROVED_USERS = process.env['COUNT_UNAPPROVED_USERS'] === 'true';
-var DROP_POSEIDON = process.env['DROP_POSEIDON_REQUESTS'] === 'true';
-var MALFORMED_LIMIT = process.env['MALFORMED_LIMIT'] || '0';
+var ADMIN_USER = process.env.ADMIN_USER || 'poseidon';
+var BILLABLE_OPS = (process.env.BILLABLE_OPS).split(' ');
+var EXCLUDE_UNAPPROVED_USERS = process.env.EXCLUDE_UNAPPROVED_USERS === 'true';
+var INCLUDE_ADMIN_REQUESTS = process.env.INCLUDE_ADMIN_REQUESTS === 'true';
+var LOOKUP_PATH = process.env.LOOKUP_FILE || '../etc/lookup.json';
+var MALFORMED_LIMIT = process.env.MALFORMED_LIMIT || '0';
 
-var LOG = require('bunyan').createLogger({
-    name: 'request-map.js',
-    stream: process.stderr,
-    level: process.env['LOG_LEVEL'] || 'info'
-});
 
-function shouldProcess(record) {
-    return (record.audit &&
-        record.req.url !== '/ping' &&
-        typeof (record.req.owner) !== 'undefined' &&
-        (!record.req.caller ||
-            !DROP_POSEIDON ||
-            record.req.caller.login !== 'poseidon'));
+function RequestMapStream(opts) {
+    this.admin = opts.admin;
+    this.billableOps = opts.billableOps;
+    this.includeAdmin = opts.includeAdmin;
+    this.excludeUnapproved = opts.excludeUnapproved;
+    this.log = opts.log;
+    this.lookup = opts.lookup;
+
+    this.lineNumber = 0;
+    this.aggr = {};
+    opts.decodeStrings = false;
+    Transform.call(this, opts);
 }
-
-function okStatus(code) {
-    return (code >= 200 && code <= 204);
-}
+util.inherits(RequestMapStream, Transform);
 
 
-function count(record, aggr) {
-    var owner = record.req.owner;
-    var operation = record.billable_operation;
-    var method = record.req.method;
-    var resHeaderLength = record.resHeaderLength;
-    var reqHeaderLength = record.reqHeaderLength;
-    var statusCode = record.res.statusCode;
+RequestMapStream.prototype._transform = function _transform(line, enc, cb) {
+    var self = this;
+    this.lineNumber++;
 
-    // only count successful requests
-    if (!okStatus(statusCode)) {
+    // since bunyan logs may contain lines such as
+    // [ Nov 28 21:35:27 Enabled. ]
+    // we need to ignore them
+    if (line[0] !== '{') {
+        cb();
         return;
     }
 
-    // get the content-length if it exists
-    var contentLength = 0;
-    if (record.res.headers && record.res.headers['content-length']) {
-        contentLength = +record.res.headers['content-length'];
-    } else if (record.req.headers && record.req.headers['content-length']) {
-        contentLength = +record.req.headers['content-length'];
-    } else if (record.bytesTranferred) {
-        // bytesTransferred will exist if the request is streaming
-        // i.e. transfer-encoding: chunked
-        contentLength = +record.bytesTransferred;
+    var record;
+    try {
+        record = JSON.parse(line);
+    } catch (e) {
+        // Sometimes a log will be rotated in the middle of a bunyan entry,
+        // which will cause JSON.parse to fail. We'll ignore those, but we want
+        // to keep track of them in case there's a more widespread issue with
+        // record parsing.
+        this.emit('malformed', line);
+        this.log.warn({
+            error: e,
+            line: line
+        }, 'error parsing line ' + this.lineNumber);
     }
 
-    aggr[owner] = aggr[owner] || {
-        owner: owner,
-        requests: {
-            type: {
-                // if these change, update summarize-reduce.js
-                DELETE: 0,
-                GET: 0,
-                HEAD: 0,
-                LIST: 0,
-                OPTIONS: 0,
-                POST: 0,
-                PUT: 0
-            },
+    if (!this._shouldProcess(record)) {
+        cb();
+        return;
+    }
+
+    // initialize the user
+    var owner = record.req.owner;
+    var method = record.req.method;
+    if (!this.aggr[owner]) {
+        this.aggr[owner] = {
+            owner: owner,
+            requests: {},
             bandwidth: {
                 in: new Big(0),
                 out: new Big(0),
                 headerIn: new Big(0),
                 headerOut: new Big(0)
             }
-        }
-    };
-
-    if (operation) {
-        aggr[owner].requests.type[operation]++;
+        };
+        this.billableOps.forEach(function (op) {
+            self.aggr[owner].requests[op] = 0;
+        });
     }
 
-    var bw = aggr[owner].requests.bandwidth;
+    var operation = record.billable_operation;
+    if (operation) {
+        this.aggr[owner].requests.type[operation]++;
+    }
+
+    // count header bandwidth
+    var bw = this.aggr[owner].requests.bandwidth;
+    var resHeaderLength = record.resHeaderLength;
+    var reqHeaderLength = record.reqHeaderLength;
     bw.headerIn = bw.headerIn.plus(reqHeaderLength);
     bw.headerOut = bw.headerOut.plus(resHeaderLength);
 
+    // get the content-length if it exists
+    var contentLength = 0;
+    if (record.res.headers && record.res.headers['content-length']) {
+        contentLength = parseInt(record.res.headers['content-length'], 10);
+    } else if (record.req.headers && record.req.headers['content-length']) {
+        contentLength = parseInt(record.req.headers['content-length'], 10);
+    } else if (record.bytesTranferred) {
+        // bytesTransferred will exist if the request is streaming
+        // i.e. transfer-encoding: chunked
+        contentLength = parseInt(record.bytesTransferred, 10);
+    }
     // only count bandwidth for GET & PUT
     if (method === 'GET') {
         bw.out = bw.out.plus(contentLength);
     }
-
     if (method === 'PUT') {
         bw.in = bw.in.plus(contentLength);
     }
-}
+    cb();
+};
 
-function printResults(aggr) {
-    Object.keys(aggr).forEach(function (owner) {
-        console.log(JSON.stringify(aggr[owner], function (key, value) {
-            if (value instanceof Big) {
-                return (value.toString());
-            }
-            return (value);
-        }));
+
+RequestMapStream.prototype._shouldProcess = function _shouldProcess(record) {
+    var isAudit = record.audit;
+    var isPing = record.req.url === '/ping';
+    var hasOwner = typeof (record.req.owner) !== 'undefined';
+    var okStatus = record.res.statusCode >= 200 && record.res.statusCode <= 299;
+    var isAdmin = record.req.caller && record.req.caller.login === this.admin;
+
+    var isApproved;
+    if (this.excludeUnapproved) {
+        if (!this.lookup[record.req.owner]) {
+            this.log.warn({
+                record: record
+            }, 'No login found for %s', record.req.owner);
+            isApproved = true;
+        } else {
+            isApproved = this.lookup[record.req.owner].approved;
+        }
+    } else {
+        isApproved = true;
+    }
+
+    return (isAudit &&
+            !isPing &&
+            hasOwner &&
+            okStatus &&
+            isApproved &&
+            (this.includeAdmin || !isAdmin));
+};
+
+
+RequestMapStream.prototype._flush = function _flush(cb) {
+    var self = this;
+
+    function bigToString(key, value) {
+        if (value instanceof Big) {
+            return (value.toString());
+        }
+        return (value);
+    }
+
+    Object.keys(this.aggr).forEach(function (owner) {
+        self.push(JSON.stringify(self.aggr[owner], bigToString) + '\n');
     });
-}
+    cb();
+};
+
+
 
 function main() {
-    var carry = mod_carrier.carry(process.openStdin());
-    var aggr = {};
-    var lineCount = 0;
-    var malformed = 0;
-    carry.on('line', function onLine(line) {
-        lineCount++;
-        var record;
-
-        // since bunyan logs may contain lines such as
-        // [ Nov 28 21:35:27 Enabled. ]
-        // we need to ignore them
-        if (line[0] != '{') {
-            return;
-        }
-
-        try {
-            record = JSON.parse(line);
-        } catch (e) {
-            malformed++;
-            LOG.error(e, 'Error on line ' + lineCount);
-            return;
-        }
-
-        // only process audit records, and ignore pings
-        if (!shouldProcess(record)) {
-            return;
-        }
-
-        if (!COUNT_UNAPPROVED_USERS) {
-            if (!lookup[record.req.owner]) {
-                LOG.error(record, 'No login found for UUID ' +
-                    record.req.owner);
-                ERROR = true;
-                return;
-            }
-
-            if (!lookup[record.req.owner].approved) {
-                LOG.warn(record, record.req.owner +
-                    ' not approved for provisioning. ' +
-                    'Skipping...');
-                return;
-            }
-        }
-
-        count(record, aggr);
+    var log = require('bunyan').createLogger({
+        name: 'storage-reduce1.js',
+        stream: process.stderr,
+        level: process.env.LOG_LEVEL || 'info'
     });
 
-    carry.once('end', function onEnd() {
+    var lookup;
+
+    if (EXCLUDE_UNAPPROVED_USERS) {
+        lookup = require(LOOKUP_PATH);
+    }
+
+    var mapStream = new RequestMapStream({
+        admin: ADMIN_USER,
+        billableOps: BILLABLE_OPS,
+        excludeUnapproved: EXCLUDE_UNAPPROVED_USERS,
+        includeAdmin: INCLUDE_ADMIN_REQUESTS,
+        log: log,
+        lookup: lookup
+    });
+
+    var malformed = 0;
+
+    mapStream.once('error', function (error) {
+        log.fatal({error: error}, 'request map phase error');
+        process.exit(1);
+    });
+
+    mapStream.on('malformed', function () {
+        malformed++;
+    });
+
+    mapStream.once('end', function () {
         var len = MALFORMED_LIMIT.length;
         var threshold;
 
         if (MALFORMED_LIMIT[len - 1] === '%') {
             var pct = +(MALFORMED_LIMIT.substr(0, len-1));
-            threshold = pct * lineCount;
+            threshold = pct * mapStream.lineNumber;
         } else {
-            threshold = +MALFORMED_LIMIT;
+            threshold = parseInt(MALFORMED_LIMIT, 10);
         }
 
         if (isNaN(threshold)) {
-            LOG.error('MALFORMED_LIMIT not a number');
-            ERROR = true;
             return;
         }
 
         if (malformed > threshold) {
-            LOG.fatal('Too many malformed lines');
-            ERROR = true;
+            log.fatal({count: malformed}, 'Too many malformed lines.');
+            process.exit(1);
             return;
         }
-
-        printResults(aggr);
     });
+
+    process.stdin.pipe(new lstream()).pipe(mapStream).pipe(process.stdout);
 }
 
 if (require.main === module) {
-
-    process.on('exit', function onExit() {
-        process.exit(ERROR);
-    });
-
     main();
 }
+
+module.exports = RequestMapStream;
